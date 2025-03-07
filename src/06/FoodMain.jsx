ba0edab9310dc4872d7983439c64a19d258d7487
ohyeon1002 <skyblueazure4@gmail.com>
2025-03-07 09:12:36 +0900

    푸드 진짜끝

--- src/06/FoodMain.jsx
+++ src/06/FoodMain.jsx
@@ -4,15 +4,17 @@ import TypeChecker from "./TypeChecker";
 import { useState } from "react";
 
 export default function FoodMain() {
-    const [show, setShow] = useState("All");
-    const re = new RegExp("\\d{1}\\.\\s*");
-    const a = new Set(["All", ... Fooddata.flatMap(item => item['운영주체 분류'].replace(re, ""))]); console.log(a);
+    const [show, setShow] = useState("All"); //useState 설정
+    const re = new RegExp("\\d{1}\\.\\s*"); //정규표현식
+    const a = new Set(["All", ... Fooddata.flatMap(item => item['운영주체 분류'].replace(re, ""))]); //운영주체명 문자만 뽑아서 셋에 넣기+ALL추가
 
-    const tags = (show == "All")
+    const ck = Array.from(a).map(item => <TypeChecker key={item} value={item} chk={(c) => setShow(c)} />); //셋을 바탕으로 만든 배열을 매핑해 라디오버튼 컴포넌트 생성
+
+    const tags = (show == "All") //삼항연산자로 상태가 ALL일때와 아닐때 표시할 컴포넌트 구분
     ? Fooddata.map(item => <FoodCard key={item['사업장명']} obj={item} />)
     : Fooddata.filter(item => item['운영주체 분류'].endsWith(show)).map(item => <FoodCard key={item['사업장명']} obj={item} />);
     
-    const ck = Array.from(a).map(item => <TypeChecker key={item} value={item} chk={(c) => setShow(c)} />)
+
   return (
     <>
     <div className="inline-flex border-1 border-dashed m-3 justify-self-start">
